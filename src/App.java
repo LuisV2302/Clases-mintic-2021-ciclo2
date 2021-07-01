c0a4777e0dbe1c3f37fe5400cc9b2a686f6baf97
Luis V <luesvilo2314@gmail.com>
2021-07-01 17:49:05 -0500

    practicas de clases

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int variable = 8;
+        boolean  variable2 = true;
+        String variable3 = "Luis";
+        float variable4 = 2.4f;
+        double variable5 = 3.456;
+        char variable6 = 'j';
+        System.out.println(variable);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+    }
+}
