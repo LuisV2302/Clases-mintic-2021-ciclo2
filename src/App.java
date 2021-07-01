11966d4171d3723949e97fd5c0195b1095efe726
LuisV2302 <luesvilo2314@gmail.com>
2021-07-01 18:06:32 -0500

    linea borrada como prueba

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         int variable = 8;
         boolean  variable2 = true;
         String variable3 = "Luis";
